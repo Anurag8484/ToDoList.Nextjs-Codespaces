944c5d135998564340b84f7ffa6befc7f8938f6d
Anurag Poonia <23f1002560@ds.study.iitm.ac.in>
2025-01-25 16:25:44 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -61,6 +61,8 @@ export default function Home() {
 
       <footer>
         <Link href="/about">About</Link>
+        <p>Contact me at: <!--email_off-->23f1002560@ds.study.iitm.ac.in<!--/email_off--></p>
+
       </footer>
     </div>
   );
