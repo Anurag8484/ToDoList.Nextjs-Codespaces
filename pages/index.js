153a76b438bdc4f370b2a02d0cd20ec74cefdb9e
Anurag Poonia <23f1002560@ds.study.iitm.ac.in>
2025-01-25 16:29:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,17 @@
-/* Create a To-Do List in Next.js
-- add items to do the list
-- delete items from the list
-*/
-
 import React, { useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { v4 as uuidv4 } from 'uuid';
 
-
 export default function Home() {
-  //  creates state for the to-do list with default items like: learn next.js, learn react.js, learn copilot
   const [todoList, setTodoList] = useState([
     { id: uuidv4(), item: 'learn next.js' },
     { id: uuidv4(), item: 'learn react.js' },
     { id: uuidv4(), item: 'learn copilot' }
   ]);
 
-  //  creates state for to do items
   const [todoItem, setTodoItem] = useState('');
-  
-  //  function that handels  adding items to the to-do list
+
   const addTodoItem = () => {
     if (todoItem) {
       setTodoList([...todoList, { id: uuidv4(), item: todoItem }]);
@@ -28,12 +19,10 @@ export default function Home() {
     }
   };
 
-  //  function that handels deleting items from the to-do list
   const deleteTodoItem = (id) => {
     setTodoList(todoList.filter((todo) => todo.id !== id));
   };
-    
-  // render input field, button, and list of to-do items
+
   return (
     <div>
       <Head>
@@ -62,11 +51,7 @@ export default function Home() {
       <footer>
         <Link href="/about">About</Link>
         <p>Contact me at: <!--email_off-->23f1002560@ds.study.iitm.ac.in<!--/email_off--></p>
-
       </footer>
     </div>
   );
-
-
-
-}  
+}
